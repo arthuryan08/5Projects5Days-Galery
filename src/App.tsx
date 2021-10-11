1475d682dc0ccc694ff5fe25c89fccf177967246
Arthur Yan <arthuryan08@gmail.com>
2021-10-10 21:22:36 -0300

    ending uploading function

--- src/App.tsx
+++ src/App.tsx
@@ -24,14 +24,22 @@ const App = () => {
 
     const formData = new FormData(e.currentTarget)
     const file = formData.get('image') as File;
+    
     if(file && file.size > 0) {
       setUploading(true);
-      // faz o envio do arquivo
+      let result = await Photos.insert(file);
       setUploading(false);
+
+      if(result instanceof Error){
+        alert(`${result.name} - ${result.message}`)
+      } else {
+        let newPhotoList = [...photos];
+        newPhotoList.push(result);
+        setPhotos(newPhotoList)
+      }
     }
   }
 
-  console.log(photos)
   return (
     <C.Container>
       <C.Area>
@@ -40,6 +48,7 @@ const App = () => {
         <C.UploadForm method="POST" onSubmit={handleFormSubmit}>
           <input type="file" name="image" />
           <input type="submit" value="Enviar" />
+          {uploading && "Enviando..."}
         </C.UploadForm>
 
         {loading &&
